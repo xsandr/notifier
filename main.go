7c077dfef4b035a10ea649a05bbd822a8c69291b
Aleksandr Karasev <xsandr.k@gmail.com>
2014-11-09 20:18:01 +0300

    Check origin

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ var (
 	upgrader  = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
-		CheckOrigin:     func(r *http.Request) bool { return true },
+		CheckOrigin:     nil,
 	}
 )
 
